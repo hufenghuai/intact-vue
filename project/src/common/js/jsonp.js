0ec3691c8c5903b3807ec614fc3dba7d12ffbb6b
wb-hufenghuai <13521858457@163.com>
2018-04-24 17:48:24 +0800

    first mistake

--- project/src/common/js/jsonp.js
+++ project/src/common/js/jsonp.js
@@ -1,4 +1,4 @@
-import originJSONP from 'common/js/jsonp'
+import originJSONP from 'jsonp'
 
 export default function jsonp (url, data, option) {
   url += (url.indexOf('?') < 0 ? '&' : '?') + param(data)
