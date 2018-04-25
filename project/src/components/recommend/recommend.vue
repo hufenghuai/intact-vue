a5152c26d54ac7bf2643927091735a966b7db962
wb-hufenghuai <13521858457@163.com>
2018-04-25 15:23:20 +0800

    second day break

--- project/src/components/recommend/recommend.vue
+++ project/src/components/recommend/recommend.vue
@@ -1,33 +1,139 @@
 <template>
-  <div class="recommend">推荐</div>
+  <div class="recommend">
+    <scroll ref="scroll" :data="discList" class="recommend-content">
+      <div>
+        <div class="slider-wrapper">
+          <div class="slider-content">
+            <slider  v-if="recommends.length">
+              <div :key="index" v-for="(item, index) in recommends">
+                <a :href="item.linkUrl">
+                  <img @load="loadImage" :src="item.picUrl" >
+                </a>
+              </div>
+            </slider>
+          </div>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li class="item" :key="index" v-for="(item, index) in discList">
+              <div class="icon">
+                <img v-lazy="item.imgurl" width="60" height="60" alt="">
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </scroll>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import { getRecommend } from 'api/recommend'
+  import Slider from 'base/slider/slider'
+  import Scroll from 'base/scroll/scroll'
+  import { getRecommend, getDiscList } from 'api/recommend'
   import { ERR_OK } from 'api/config'
 
   export default {
     data () {
       return {
-        recommends: []
+        recommends: [],
+        discList: []
       }
     },
     created () {
       this._getRecommend()
+      this._getDiscList()
     },
     methods: {
       _getRecommend () {
         getRecommend().then((res) => {
           if (res.code === ERR_OK) {
-            this.recommends = res.data
-            console.log(this.recommends)
+            this.recommends = res.data.slider
           }
         })
+      },
+      _getDiscList () {
+        getDiscList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.discList = res.data.list
+          }
+        })
+      },
+      loadImage () {
+        if (!this.checkLoaded) {
+          return
+        }
+        this.$refs.scroll.refresh()
+        this.checkLoaded = true
       }
+    },
+    components: {
+      Slider,
+      Scroll
     }
   }
 </script>
 
 <style lang="stylus" ref="stylesheet/stylus">
+  @import "~common/stylus/variable"
 
+  .recommend
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+    .recommend-content
+      height: 100%
+      overflow: hidden
+      .slider-wrapper
+        position: relative
+        width: 100%
+        height: 0
+        padding-top: 40%
+        overflow: hidden
+        .slider-content
+          position: absolute
+          top: 0
+          left: 0
+          width: 100%
+          height: 100%
+      .recommend-list
+        .list-title
+          height: 65px
+          line-height: 65px
+          text-align: center
+          font-size: $font-size-medium
+          color: $color-theme
+        .item
+          display: flex
+          box-sizing: border-box
+          align-items: center
+          padding: 0 20px 20px 20px
+          .icon
+            flex: 0 0 60px
+            width: 60px
+            padding-right: 20px
+          .text
+            display: flex
+            flex-direction: column
+            justify-content: center
+            flex: 1
+            line-height: 20px
+            overflow: hidden
+            font-size: $font-size-medium
+            .name
+              margin-bottom: 10px
+              color: $color-text
+            .desc
+              color: $color-text-d
+      .loading-container
+        position: absolute
+        width: 100%
+        top: 50%
+        transform: translateY(-50%)
 </style>
--- project/src/main.js
+++ project/src/main.js
@@ -3,11 +3,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclcik from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 import 'common/stylus/index.styl'
 
 fastclcik.attach(document.body)
 
 Vue.config.productionTip = false
+
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
 
 /* eslint-disable no-new */
 new Vue({
